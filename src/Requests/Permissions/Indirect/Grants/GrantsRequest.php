6747cac879af0929e143b12198665253ab4c87e0
N1ebieski <kontakt@intelekt.net.pl>
2025-11-06 18:13:12 +0100

    Add permissions/*/grants endpoints Fixes #30

--- src/Requests/Permissions/Indirect/Grants/GrantsRequest.php
+++ src/Requests/Permissions/Indirect/Grants/GrantsRequest.php
@@ -12,6 +12,7 @@ use N1ebieski\KSEFClient\DTOs\Requests\Permissions\TargetIdentifierInternalIdGro
 use N1ebieski\KSEFClient\DTOs\Requests\Permissions\TargetIdentifierNipGroup;
 use N1ebieski\KSEFClient\DTOs\Requests\Permissions\TargetIdentifierTypeGroup;
 use N1ebieski\KSEFClient\Requests\AbstractRequest;
+use N1ebieski\KSEFClient\Support\Optional;
 use N1ebieski\KSEFClient\ValueObjects\Requests\Description;
 use N1ebieski\KSEFClient\ValueObjects\Requests\Permissions\Indirect\IndirectPermissionType;
 
@@ -22,9 +23,9 @@ final class GrantsRequest extends AbstractRequest implements BodyInterface
      */
     public function __construct(
         public readonly SubjectIdentifierNipGroup | SubjectIdentifierPeselGroup | SubjectIdentifierFingerprintGroup $subjectIdentifierGroup,
-        public readonly TargetIdentifierNipGroup | TargetIdentifierInternalIdGroup | TargetIdentifierTypeGroup $targetIdentifierGroup,
         public readonly array $permissions,
-        public readonly Description $description
+        public readonly Description $description,
+        public readonly Optional | TargetIdentifierNipGroup | TargetIdentifierInternalIdGroup | TargetIdentifierTypeGroup $targetIdentifierGroup = new Optional(),
     ) {
     }
 
@@ -33,18 +34,21 @@ final class GrantsRequest extends AbstractRequest implements BodyInterface
         /** @var array<string, mixed> $data */
         $data = $this->toArray();
 
+        if ( ! $this->targetIdentifierGroup instanceof Optional) {
+            $data['targetIdentifier'] = [
+                'type' => $this->targetIdentifierGroup->getIdentifier()->getType(),
+                ...( ! $this->targetIdentifierGroup instanceof TargetIdentifierTypeGroup ? [
+                    'value' => (string) $this->targetIdentifierGroup->getIdentifier()
+                ] : []),
+            ];
+        }
+
         return [
             ...$data,
             'subjectIdentifier' => [
                 'type' => $this->subjectIdentifierGroup->getIdentifier()->getType(),
                 'value' => (string) $this->subjectIdentifierGroup->getIdentifier(),
-            ],
-            'targetIdentifier' => [
-                'type' => $this->targetIdentifierGroup->getIdentifier()->getType(),
-                ...( ! $this->targetIdentifierGroup instanceof TargetIdentifierTypeGroup ? [
-                    'value' => (string) $this->targetIdentifierGroup->getIdentifier()
-                ] : []),
-            ],
+            ]
         ];
     }
 }
--- tests/Feature/Resources/Permissions/Entities/EntitiesResourceTest.php
+++ tests/Feature/Resources/Permissions/Entities/EntitiesResourceTest.php
@@ -12,20 +12,22 @@ use N1ebieski\KSEFClient\ValueObjects\Requests\Testdata\Subject\SubjectType;
 
 /** @var AbstractTestCase $this */
 
-beforeEach(function (): void {
+beforeAll(function (): void {
     $client = (new ClientBuilder())
         ->withMode(Mode::Test)
         ->build();
 
-    try {
-        $client->testdata()->subject()->create([
-            'subjectNip' => $_ENV['NIP_2'],
-            'subjectType' => SubjectType::EnforcementAuthority,
-            'description' => 'Subject who gives InvoiceWrite permission',
-        ])->status();
-    } catch (BadRequestException $exception) {
-        if (str_starts_with($exception->getMessage(), '30001')) {
-            // ignore
+    foreach (['NIP_2', 'NIP_3'] as $nip) {
+        try {
+            $client->testdata()->subject()->create([
+                'subjectNip' => $_ENV[$nip],
+                'subjectType' => SubjectType::EnforcementAuthority,
+                'description' => 'Subject who gives InvoiceWrite permission',
+            ])->status();
+        } catch (BadRequestException $exception) {
+            if (str_starts_with($exception->getMessage(), '30001')) {
+                // ignore
+            }
         }
     }
 });
@@ -35,12 +37,14 @@ afterAll(function (): void {
         ->withMode(Mode::Test)
         ->build();
 
-    $client->testdata()->subject()->remove([
-        'nip' => $_ENV['NIP_2'],
-    ]);
+    foreach (['NIP_2', 'NIP_3'] as $nip) {
+        $client->testdata()->subject()->remove([
+            'nip' => $_ENV['NIP_2'],
+        ]);
+    }
 });
 
-test('give InvoiceWrite permission and send invoice as NIP_2', function (): void {
+test('send invoice as NIP_2 when NIP_2 gave InvoiceWrite permission', function (): void {
     /** @var AbstractTestCase $this */
     /** @var array<string, string> $_ENV */
 
@@ -159,3 +163,152 @@ test('give InvoiceWrite permission and send invoice as NIP_2', function (): void
     $this->revokeCurrentSession($clientNip1);
     $this->revokeCurrentSession($clientNip2);
 });
+
+test('send invoice as NIP_3 when NIP_3 gave canDelegate InvoiceWrite permission', function (): void {
+    /** @var AbstractTestCase $this */
+    /** @var array<string, string> $_ENV */
+
+    $clientNip3 = $this->createClient(
+        identifier: $_ENV['NIP_3'],
+        certificatePath: $_ENV['CERTIFICATE_PATH_3'],
+        certificatePassphrase: $_ENV['CERTIFICATE_PASSPHRASE_3']
+    );
+
+    $grantsResponse = $clientNip3->permissions()->entities()->grants([
+        'subjectIdentifierGroup' => [
+            'nip' => $_ENV['NIP_2']
+        ],
+        'permissions' => [
+            [
+                'type' => 'InvoiceWrite',
+                'canDelegate' => true
+            ]
+        ],
+        'description' => 'Give InvoiceWrite permission to NIP_2'
+    ])->object();
+
+    Utility::retry(function (int $attempts) use ($clientNip3, $grantsResponse) {
+        $statusResponse = $clientNip3->permissions()->operations()->status([
+            'referenceNumber' => $grantsResponse->referenceNumber,
+        ])->object();
+
+        try {
+            expect($statusResponse->status->code)->toBe(200);
+
+            return $statusResponse;
+        } catch (Throwable $exception) {
+            if ($attempts > 2) {
+                throw $exception;
+            }
+        }
+    });
+
+    $clientNip2 = $this->createClient(
+        identifier: $_ENV['NIP_2'],
+        certificatePath: $_ENV['CERTIFICATE_PATH_2'],
+        certificatePassphrase: $_ENV['CERTIFICATE_PASSPHRASE_2']
+    );
+
+    $grantsResponse = $clientNip2->permissions()->indirect()->grants([
+        'subjectIdentifierGroup' => [
+            'nip' => $_ENV['NIP_1']
+        ],
+        'targetIdentifierGroup' => [
+            'nip' => $_ENV['NIP_3']
+        ],
+        'permissions' => [
+            'InvoiceWrite'
+        ],
+        'description' => 'Give NIP_3 InvoiceWrite permission to NIP_1'
+    ])->object();
+
+    Utility::retry(function (int $attempts) use ($clientNip2, $grantsResponse) {
+        $statusResponse = $clientNip2->permissions()->operations()->status([
+            'referenceNumber' => $grantsResponse->referenceNumber,
+        ])->object();
+
+        try {
+            expect($statusResponse->status->code)->toBe(200);
+
+            return $statusResponse;
+        } catch (Throwable $exception) {
+            if ($attempts > 2) {
+                throw $exception;
+            }
+        }
+    });
+
+    $encryptionKey = EncryptionKeyFactory::makeRandom();
+
+    $clientNip1 = $this->createClient(
+        identifier: $_ENV['NIP_3'],
+        encryptionKey: $encryptionKey
+    );
+
+    $openResponse = $clientNip1->sessions()->online()->open([
+        'formCode' => 'FA (3)',
+    ])->object();
+
+    $fakturaFixture = (new FakturaSprzedazyTowaruFixture())
+        ->withNip($_ENV['NIP_3'])
+        ->withTodayDate()
+        ->withRandomInvoiceNumber();
+
+    $fixture = (new SendRequestFixture())->withFakturaFixture($fakturaFixture);
+
+    $sendResponse = $clientNip1->sessions()->online()->send([
+        ...$fixture->data,
+        'referenceNumber' => $openResponse->referenceNumber,
+    ])->object();
+
+    $clientNip1->sessions()->online()->close([
+        'referenceNumber' => $openResponse->referenceNumber
+    ]);
+
+    Utility::retry(function (int $attempts) use ($clientNip1, $openResponse, $sendResponse) {
+        $statusResponse = $clientNip1->sessions()->invoices()->status([
+            'referenceNumber' => $openResponse->referenceNumber,
+            'invoiceReferenceNumber' => $sendResponse->referenceNumber
+        ])->object();
+
+        try {
+            expect($statusResponse->status->code)->toBe(200);
+
+            return $statusResponse;
+        } catch (Throwable $exception) {
+            if ($attempts > 2) {
+                throw $exception;
+            }
+        }
+    });
+
+    $queryResponse = $clientNip1->permissions()->query()->personal()->grants([
+        'targetIdentifierGroup' => [
+            'nip' => $_ENV['NIP_3']
+        ],
+    ])->object();
+
+    $revokePermissionResponse = $clientNip2->permissions()->common()->revoke([
+        'permissionId' => $queryResponse->permissions[0]->id
+    ])->object();
+
+    Utility::retry(function (int $attempts) use ($clientNip2, $revokePermissionResponse) {
+        $statusResponse = $clientNip2->permissions()->operations()->status([
+            'referenceNumber' => $revokePermissionResponse->referenceNumber,
+        ])->object();
+
+        try {
+            expect($statusResponse->status->code)->toBe(200);
+
+            return $statusResponse;
+        } catch (Throwable $exception) {
+            if ($attempts > 2) {
+                throw $exception;
+            }
+        }
+    });
+
+    $this->revokeCurrentSession($clientNip1);
+    $this->revokeCurrentSession($clientNip2);
+    $this->revokeCurrentSession($clientNip3);
+})->only();
