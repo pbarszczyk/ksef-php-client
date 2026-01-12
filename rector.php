e1bf965af6de6cb7be232e9ba8f8bed8c2821f7d
Piotr Barszczyk <piotr.barszczyk95@gmail.com>
2026-01-12 18:53:27 +0100

    Init fork commit

--- rector.php
+++ rector.php
@@ -2,32 +2,48 @@
 
 declare(strict_types=1);
 
-return \Rector\Config\RectorConfig::configure()
+use N1ebieski\KSEFClient\Overrides\Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Carbon\Rector\MethodCall\DateTimeMethodCallToCarbonRector;
+use Rector\Carbon\Rector\New_\DateTimeInstanceToCarbonRector;
+use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\FuncCall\FunctionFirstClassCallableRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector;
+
+return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
     ->withRules([
-        \N1ebieski\KSEFClient\Overrides\Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,
+        ClosureToArrowFunctionRector::class,
     ])
     ->withSkip([
-        \Rector\Carbon\Rector\MethodCall\DateTimeMethodCallToCarbonRector::class,
-        \Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector::class,
-        \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
+        DateTimeMethodCallToCarbonRector::class,
+        RemoveNonExistingVarAnnotationRector::class,
+        EncapsedStringsToSprintfRector::class,
         \Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,
-        \Rector\Carbon\Rector\New_\DateTimeInstanceToCarbonRector::class,
-        \Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector::class => [
+        DateTimeInstanceToCarbonRector::class,
+        ExplicitBoolCompareRector::class => [
             __DIR__ . '/src/Actions/ConvertEcdsaDerToRaw/ConvertEcdsaDerToRawHandler.php'
         ],
-        \Rector\CodingStyle\Rector\FuncCall\FunctionFirstClassCallableRector::class => [
+        FunctionFirstClassCallableRector::class => [
             __DIR__ . '/src/Validator/Rules/Number/NipRule.php'
         ],
-        \Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector::class => [
+        CompleteDynamicPropertiesRector::class => [
             __DIR__ . '/src/Testing/Fixtures/Requests/AbstractResponseFixture.php',
             __DIR__ . '/src/Testing/Fixtures/DTOs/Requests/Sessions/AbstractFakturaFixture.php'
         ],
-        \Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector::class
+        CatchExceptionNameMatchingTypeRector::class
     ])
+    ->withCache(
+        __DIR__.'/.rector.cache',
+        FileCacheStorage::class
+    )
     ->withComposerBased(phpunit: true)
     ->withImportNames(removeUnusedImports: true)
     ->withPreparedSets(
